86922fd9e3f4a4341edf7d4cd582845dfebfaa73
Ojas Srivastava <srivastavaojas454@gmail.com>
2025-04-25 17:15:39 +0530

    removed calendar feature

--- calendar/mycal/calendarapp/forms.py
+++
@@ -1,8 +0,0 @@
-# calendarapp/forms.py
-from django import forms
-from .models import Event
-
-class EventForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ['title', 'description', 'start_time', 'end_time', 'location', 'all_day']
